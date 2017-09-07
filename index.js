f16f593c9d4ccdd0eaf9281f2442183927d6e5dc
Mila Hose <mila.hose@gmail.com>
2017-09-06 18:00:02 -0700

    Add node server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-require('./app/index');
+const http = require('http');
+
+http.createServer((request, response) => {
+  console.log('request url:', request.url);
+  response.end('Hello World!');
+}).listen(3000, (port, err) => {
+  if (err) throw err;
+  console.log('Listening on port 3000');
+});
